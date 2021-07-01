1fd20279115af925a6e52754e66932dc5f6fdc89
Rajan Gupta <guptarajan698@gmaail.com>
2021-07-02 04:50:03 +0530

    spacex lunchpad data shown in google mapi

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { DataServiceService } from '../service/data-service.service';
-// import { MouseEvent } from '@agm/core';
 import { MapsAPILoader } from '@agm/core';
 
 // just an interface for type safety.
 interface marker {
 	lat: number;
 	lng: number;
-	label?: string;
+	label?: any;
 	draggable: boolean;
 }
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -19,31 +17,17 @@ interface marker {
 
 export class HomeComponent implements OnInit {
 
-  title = 'My first AGM project';
-  lat = 51.678418;
-  lng = 7.809007;
+  title = 'All Launchpads';
+  lat:number = 51.678418;
+  lng:number = 7.809007;
+  zoom: number = 1;
 
-  markers: marker[] = [
-	  {
-		  lat: 51.673858,
-		  lng: 7.815982,
-		  label: 'A',
-		  draggable: true
-	  },
-	  {
-		  lat: 51.373858,
-		  lng: 7.215982,
-		  label: 'B',
-		  draggable: false
-	  },
-	  {
-		  lat: 51.723858,
-		  lng: 7.895982,
-		  label: 'C',
-		  draggable: true
-	  }
-  ]
- 
+  launchpadList:any[] = [];
+  launchesList:any[] = [];
+
+  markers: marker[] = [];
+
+
   constructor(
     private dataService: DataServiceService) { }
 
@@ -52,21 +36,19 @@ export class HomeComponent implements OnInit {
     this.getLaunchesData();
   }
 
-  // mapClicked($event: MouseEvent) {
-  //   this.markers.push({
-  //     lat: $event.coords.lat,
-  //     lng: $event.coords.lng,
-  //     draggable: true
-  //   });
-  // }
-  
-  // markerDragEnd(m: marker, $event: MouseEvent) {
-  //   console.log('dragEnd', m, $event);
-  // }
-
   getLaunchPadData () {
     this.dataService.getLaunchPadData().subscribe(res => {
       console.log('launchpad data', res)
+      this.launchpadList = res;
+      for (const launchItem of this.launchpadList) {
+        const obj =  {
+          lat: launchItem.location.latitude,
+          lng: launchItem.location.longitude,
+          label: launchItem.location.name,
+          draggable: true
+        }
+        this.markers.push(obj);
+      }
     })
   }  
 
@@ -74,6 +56,22 @@ export class HomeComponent implements OnInit {
     this.dataService.getLaunchesData().subscribe(res => {
       console.log('launches data', res)
     })
+  }
+
+  clickedMarker(label: string, index: number) {
+    console.log(`clicked the marker: ${label || index}`)
+  }
+
+  mapClicked($event: any) {
+    this.markers.push({
+      lat: $event.coords.lat,
+      lng: $event.coords.lng,
+      draggable: true
+    });
+  }
+  
+  markerDragEnd(m: marker, $event: any) {
+    console.log('dragEnd', m, $event);
   }
 
 
